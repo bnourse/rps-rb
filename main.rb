62f8a3e64e41f4619f4a884c12888c2af2d84182
Ben Nourse <bnourse@gmail.com>
2017-02-05 15:12:17 -0600

    add layout and fix

--- main.rb
+++ main.rb
@@ -1,15 +1,5 @@
 require_relative "functions.rb"
 
-# c = Choice.new
-# c.pick_weapon
-# puts c.weapon
-
-# p1 = Player.new
-# p1.init
-# p1.set_name("Player 1")
-# choice = p1.pick_choice
-# puts choice.inspect
-
 game = RPS.new
 game.init
 game.start_game
